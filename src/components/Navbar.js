dd64c9e19686eb5a4a4f5ff342dcf393365935c7
tofa93 <tofig.valiyev1993@gmail.com>
2023-05-17 23:39:18 +0200

    Added few minor changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,9 +7,9 @@ const Navbar = ({ sidenavToggle, linkSelect, activeLink }) => {
     return (
         <nav className="navbar">
             <div className="navbar-left">
-                <button className="sidenav-btn" onClick={sidenavToggle}>
+                {/* <button className="sidenav-btn" onClick={sidenavToggle}>
                     <span className="material-symbols-outlined btn-icon">menu</span>
-                </button>
+                </button> */}
                 <NavLink to="/" className={`navbar-link brand-name-link ${activeLink == '/' ? 'active' : ''}`} onClick={() => linkSelect('/')}>Recaller</NavLink>
             </div>
             <div className="navbar-right">
@@ -17,9 +17,6 @@ const Navbar = ({ sidenavToggle, linkSelect, activeLink }) => {
                     <span className="create-btn-text">Create</span>
                     <span className="material-symbols-outlined">add</span>
                 </button>
-                {/* <NavLink to="/login" className="navbar-link" id="loginLink">
-                    <span className="material-symbols-outlined">account_circle</span>
-                </NavLink> */}
                 <NavLink to="/cart" className={`navbar-link ${activeLink == '/cart' ? 'active' : ''}`} onClick={() => linkSelect('/cart')}>
                     <span className="material-symbols-outlined">shopping_cart</span>
                 </NavLink>
--- src/components/Newslist.js
+++ src/components/Newslist.js
@@ -25,26 +25,33 @@ const Newslist = ({ news }) => {
     };
 
     return (
-        <div className="news-content">
-            <Carousel
-                swipeable={false}
-                draggable={true}
-                showDots={false}
-                infinite={false}
-                responsive={responsive} containerClass='carousel-container'>
-                {news.articles.map((nws, index) => (
-                    <div className="news-block" key={index}>
-                        <div className="news-image">
-                            <img className="news-img" src={nws.urlToImage} alt="" />
+        <div className="main-content">
+            <div className="news-section">
+                <div className="news-track-title">
+                    <span className='news-track-title-text'>Trending news</span>
+                </div>
+                <Carousel
+                    swipeable={false}
+                    draggable={true}
+                    showDots={false}
+                    infinite={false}
+                    responsive={responsive} containerClass='carousel-container'>
+                    {news.articles.map((nws, index) => (
+                        <div className="news-block" key={index}>
+                            <div className="news-image">
+                                <a href={nws.url} target='_blank'>
+                                    <img className="news-img" src={nws.urlToImage} alt="news-image" />
+                                </a>
+                            </div>
+                            <div className="news-body">
+                                <p className="news-title">{nws.title}</p>
+                                <span className="news-publicsher">{nws.source.name}</span>
+                                {/* <span className="news-date">{nws.publishedAt.split("T")[0]}</span> */}
+                            </div>
                         </div>
-                        <div className="news-body">
-                            <p className="news-title">{nws.title}</p>
-                            <span className="news-publicsher">{nws.source.name}</span>
-                            {/* <span className="news-date">{nws.publishedAt.split("T")[0]}</span> */}
-                        </div>
-                    </div>
-                ))}
-            </Carousel>
+                    ))}
+                </Carousel>
+            </div>
         </div>
     );
 }
