550f0e049250871dda08d9c83c6ec00645ee258a
tofa93 <tofig.valiyev1993@gmail.com>
2023-05-23 20:25:15 +0200

    Added new essential components

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -2,7 +2,11 @@ import '../styles/posts.css'
 
 const Posts = () => {
     return (
-        <div className="posts-content">Popular posts</div>
+        <div className="posts-content">
+            <div className="posts-title">Popular posts</div>
+            <div className="posts-control-panel"></div>
+            <div className="posts-results"></div>
+        </div>
     );
 }
 
