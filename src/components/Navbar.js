33f10fdb4b56fd094bfd3b178b018165777cd1c3
tofa93 <tofig.valiyev1993@gmail.com>
2023-06-08 14:12:43 +0200

    added new feature, dark mode button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,6 +4,12 @@ import { useState, useEffect } from "react";
 
 const Navbar = ({ sidenavToggle, linkSelect, activeLink }) => {
 
+    const [darkMode, setDarkMode] = useState(true);
+
+    function switchDark() {
+        setDarkMode(!darkMode);
+    }
+
     return (
         <nav className="navbar">
             <div className="navbar-left">
@@ -16,11 +22,11 @@ const Navbar = ({ sidenavToggle, linkSelect, activeLink }) => {
                 <NavLink to="/cart" className={`navbar-link ${activeLink == '/cart' ? 'active' : ''}`} onClick={() => linkSelect('/cart')}>
                     <span className="material-symbols-outlined">shopping_cart</span>
                 </NavLink>
-                <button className="create-post-btn">
-                    <span className="create-btn-text">Link</span>
-                    <span className="material-symbols-outlined">link</span>
+                <button className="theme-switch-btn" onClick={switchDark}>
+                    {darkMode && <span className="material-symbols-outlined">light_mode</span>}
+                    {!darkMode && <span className="material-symbols-outlined">dark_mode</span>}
                 </button>
-                <button className="create-post-btn">
+                <button className="use-account-btn">
                     <span className="material-symbols-outlined">person</span>
                 </button>
             </div>
