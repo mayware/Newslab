bd507b23138458e471ee6808cd6d4a8eabe16f32
tofa93 <tofig.valiyev1993@gmail.com>
2023-06-11 03:20:54 +0200

    Added new features

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
 
     const apiKey = 'AIzaSyAw1BY7lyYOCYWruxzNX_HjvJx_sxP48Uc';
 
-    const { data, error, loading } = useAxios(`https://www.googleapis.com/youtube/v3/search?key=${apiKey}&type=video&part=snippet&eventType=live&q=@NBCNews`)
+    const { data, error, loading } = useAxios(`https://www.googleapis.com/youtube/v3/search?key=${apiKey}&type=video&part=snippet&eventType=live&q=@NBCNews&maxResults=21`)
 
     return (
         <div className="content">
--- src/components/HomeData.js
+++ src/components/HomeData.js
@@ -1,11 +1,19 @@
+import '../styles/streamlist.css'
+
 const HomeData = ({ data }) => {
     return (
         <div className="stream-list">
             {data.items.map((el) => (
                 <div className="stream-video-card" key={el.id.videoId}>
-                    <a href={`https://www.youtube.com/watch?v=${el.id.videoId}`}>
-                        <img src={el.snippet.thumbnails.medium.url} alt="" />
-                    </a>
+                    <div className="video-card-poster">
+                        <a className='stream-video-link' href={`https://www.youtube.com/watch?v=${el.id.videoId}`}>
+                            <img className="video-poster-img" src={el.snippet.thumbnails.medium.url} alt="" />
+                            <div className="video-card-body">
+                                {el.snippet.title}
+                            </div>
+                        </a>
+
+                    </div>
                 </div>
             ))}
         </div>
